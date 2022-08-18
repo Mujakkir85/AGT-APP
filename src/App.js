4653f080f58044535e1ab75aa4c7ae459f23bc6b
Mujakkir85 <mujakkirlucse@gmail.com>
2022-08-18 14:08:22 +0600

    all card are added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Allcomponents/Navbar';
 import Banner from './Allcomponents/Banner';
+import BigCard from './Allcomponents/BigCard';
 
 function App() {
   return (
     <div className="font-sans m-auto">
       <Navbar></Navbar>
       <Banner></Banner>
+      <BigCard></BigCard>
     </div>
   );
 }
